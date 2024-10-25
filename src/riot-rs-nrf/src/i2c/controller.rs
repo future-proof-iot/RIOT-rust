ROMemories/ariel-os

feat: add an I2C controller abstraction

src/riot-rs-nrf/src/i2c/controller.rs
@@ -1,0 +1,214 @@
+use embassy_nrf::{
+    bind_interrupts,
+    gpio::Pin as GpioPin,
+    peripherals,
+    twim::{InterruptHandler, Twim},
+    Peripheral,
+};
+use riot_rs_shared_types::impl_async_i2c_for_driver_enum;
+
+/// I2C bus configuration.
+#[non_exhaustive]
+#[derive(Clone)]
+pub struct Config {
+    pub frequency: Frequency,
+    pub sda_pullup: bool,
+    pub scl_pullup: bool,
+    pub sda_high_drive: bool,
+    pub scl_high_drive: bool,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            frequency: Frequency::_100k,
+            sda_pullup: false,
+            scl_pullup: false,
+            sda_high_drive: false,
+            scl_high_drive: false,
+        }
+    }
+}
+
+/// I2C bus frequency.
+// NOTE(arch): the datasheets only mention these frequencies.
+#[cfg(any(context = "nrf52840", context = "nrf5340"))]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub enum Frequency {
+    /// Standard mode.
+    _100k,
+    #[cfg(context = "nrf5340")]
+    _250k,
+    /// Fast mode.
+    _400k,
+    // FIXME(embassy): the upstream Embassy crate does not support this frequency
+    // #[cfg(context = "nrf5340")]
+    // _1M,
+}
+
+impl Frequency {
+    pub const fn first() -> Self {
+        Self::_100k
+    }
+
+    pub const fn last() -> Self {
+        Self::_400k
+    }
+
+    pub const fn next(self) -> Option<Self> {
+        match self {
+            #[cfg(not(context = "nrf5340"))]
+            Self::_100k => Some(Self::_400k),
+            #[cfg(context = "nrf5340")]
+            Self::_100k => Some(Self::_250k),
+            #[cfg(context = "nrf5340")]
+            Self::_250k => Some(Self::_400k),
+            Self::_400k => None,
+        }
+    }
+
+    pub const fn prev(self) -> Option<Self> {
+        match self {
+            Self::_100k => None,
+            #[cfg(context = "nrf5340")]
+            Self::_250k => Some(Self::_100k),
+            #[cfg(not(context = "nrf5340"))]
+            Self::_400k => Some(Self::_100k),
+            #[cfg(context = "nrf5340")]
+            Self::_400k => Some(Self::_250k),
+        }
+    }
+
+    pub const fn khz(self) -> u32 {
+        match self {
+            Self::_100k => 100,
+            #[cfg(context = "nrf5340")]
+            Self::_250k => 250,
+            Self::_400k => 400,
+        }
+    }
+}
+
+riot_rs_shared_types::impl_i2c_from_frequency!();
+
+impl From<Frequency> for embassy_nrf::twim::Frequency {
+    fn from(freq: Frequency) -> Self {
+        match freq {
+            Frequency::_100k => embassy_nrf::twim::Frequency::K100,
+            #[cfg(context = "nrf5340")]
+            Frequency::_250k => embassy_nrf::twim::Frequency::K250,
+            Frequency::_400k => embassy_nrf::twim::Frequency::K400,
+        }
+    }
+}
+
+pub(crate) fn init(peripherals: &mut crate::OptionalPeripherals) {
+    // Take all I2C peripherals and do nothing with them.
+    cfg_if::cfg_if! {
+        if #[cfg(context = "nrf52840")] {
+            let _ = peripherals.TWISPI0.take().unwrap();
+            let _ = peripherals.TWISPI1.take().unwrap();
+        } else if #[cfg(context = "nrf5340")] {
+            let _ = peripherals.SERIAL0.take().unwrap();
+            let _ = peripherals.SERIAL1.take().unwrap();
+        } else {
+            compile_error!("this nRF chip is not supported");
+        }
+    }
+}
+
+macro_rules! define_i2c_drivers {
+    ($( $interrupt:ident => $peripheral:ident ),* $(,)?) => {
+        $(
+            /// Peripheral-specific I2C driver.
+            pub struct $peripheral {
+                twim: Twim<'static, peripherals::$peripheral>,
+            }
+
+            impl $peripheral {
+                #[must_use]
+                pub fn new(
+                    sda_pin: impl Peripheral<P: GpioPin> + 'static,
+                    scl_pin: impl Peripheral<P: GpioPin> + 'static,
+                    config: Config,
+                ) -> I2c {
+                    let mut twim_config = embassy_nrf::twim::Config::default();
+                    twim_config.frequency = config.frequency.into();
+                    twim_config.sda_pullup = config.sda_pullup;
+                    twim_config.scl_pullup = config.scl_pullup;
+                    twim_config.sda_high_drive = config.sda_high_drive;
+                    twim_config.scl_high_drive = config.scl_high_drive;
+
+                    bind_interrupts!(
+                        struct Irqs {
+                            $interrupt => InterruptHandler<peripherals::$peripheral>;
+                        }
+                    );
+
+                    // Make this struct a compile-time-enforced singleton: having multiple statics
+                    // defined with the same name would result in a compile-time error.
+                    paste::paste! {
+                        #[allow(dead_code)]
+                        static [<PREVENT_MULTIPLE_ $peripheral>]: () = ();
+                    }
+
+                    // FIXME(safety): enforce that the init code indeed has run
+                    // SAFETY: this struct being a singleton prevents us from stealing the
+                    // peripheral multiple times.
+                    let twim_peripheral = unsafe { peripherals::$peripheral::steal() };
+
+                    // NOTE(arch): the I2C peripheral and driver do not have any built-in timeout,
+                    // we implement it at a higher level, not in this arch-specific module.
+                    let twim = Twim::new(twim_peripheral, Irqs, sda_pin, scl_pin, twim_config);
+
+                    I2c::$peripheral(Self { twim })
+                }
+            }
+        )*
+
+        /// Peripheral-agnostic driver.
+        pub enum I2c {
+            $( $peripheral($peripheral), )*
+        }
+
+        impl embedded_hal_async::i2c::ErrorType for I2c {
+            type Error = riot_rs_shared_types::i2c::controller::Error;
+        }
+
+        impl_async_i2c_for_driver_enum!(I2c, $( $peripheral ),*);
+    }
+}
+
+// We cannot impl From because both types are external to this crate.
+fn from_error(err: embassy_nrf::twim::Error) -> riot_rs_shared_types::i2c::controller::Error {
+    use embassy_nrf::twim::Error::*;
+
+    use riot_rs_shared_types::i2c::controller::{Error, NoAcknowledgeSource};
+
+    match err {
+        TxBufferTooLong => Error::Other,
+        RxBufferTooLong => Error::Other,
+        Transmit => Error::Other,
+        Receive => Error::Other,
+        BufferNotInRAM => Error::Other,
+        AddressNack => Error::NoAcknowledge(NoAcknowledgeSource::Address),
+        DataNack => Error::NoAcknowledge(NoAcknowledgeSource::Data),
+        Overrun => Error::Overrun,
+        Timeout => Error::Timeout,
+        _ => Error::Other,
+    }
+}
+
+// FIXME: support other nRF archs
+// Define a driver per peripheral
+#[cfg(context = "nrf52840")]
+define_i2c_drivers!(
+    SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0 => TWISPI0,
+    SPIM1_SPIS1_TWIM1_TWIS1_SPI1_TWI1 => TWISPI1,
+);
+#[cfg(context = "nrf5340")]
+define_i2c_drivers!(
+    SERIAL0 => SERIAL0,
+    SERIAL1 => SERIAL1,
+);

src/riot-rs-rp/src/i2c/controller.rs
@@ -1,0 +1,198 @@
+use embassy_rp::{
+    bind_interrupts,
+    i2c::{InterruptHandler, SclPin, SdaPin},
+    peripherals, Peripheral,
+};
+use riot_rs_shared_types::impl_async_i2c_for_driver_enum;
+
+/// I2C bus configuration.
+// We do not provide configuration for internal pull-ups as the RP2040 datasheet mentions in
+// section 4.3.1.3 that the GPIO used should have pull-ups enabled.
+#[derive(Clone)]
+#[non_exhaustive]
+pub struct Config {
+    pub frequency: Frequency,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            frequency: Frequency::UpTo100k(100),
+        }
+    }
+}
+
+/// I2C bus frequency.
+// FIXME(embassy): fast mode plus is supported by hardware but requires additional configuration
+// that Embassy does not seem to currently provide.
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+#[repr(u32)]
+pub enum Frequency {
+    /// Standard mode.
+    UpTo100k(u32), // FIXME: use a range integer?
+    /// Fast mode.
+    UpTo400k(u32), // FIXME: use a range integer?
+}
+
+impl Frequency {
+    pub const fn first() -> Self {
+        Self::UpTo100k(1)
+    }
+
+    pub const fn last() -> Self {
+        Self::UpTo400k(400)
+    }
+
+    pub const fn next(self) -> Option<Self> {
+        match self {
+            Self::UpTo100k(f) => {
+                if f < 100 {
+                    // NOTE(no-overflow): `f` is small enough due to if condition
+                    Some(Self::UpTo100k(f + 1))
+                } else {
+                    Some(Self::UpTo400k(self.khz() + 1))
+                }
+            }
+            Self::UpTo400k(f) => {
+                if f < 400 {
+                    // NOTE(no-overflow): `f` is small enough due to if condition
+                    Some(Self::UpTo400k(f + 1))
+                } else {
+                    None
+                }
+            }
+        }
+    }
+
+    pub const fn prev(self) -> Option<Self> {
+        match self {
+            Self::UpTo100k(f) => {
+                if f > 1 {
+                    // NOTE(no-overflow): `f` is large enough due to if condition
+                    Some(Self::UpTo100k(f - 1))
+                } else {
+                    None
+                }
+            }
+            Self::UpTo400k(f) => {
+                if f > 100 + 1 {
+                    // NOTE(no-overflow): `f` is large enough due to if condition
+                    Some(Self::UpTo400k(f - 1))
+                } else {
+                    Some(Self::UpTo100k(self.khz() - 1))
+                }
+            }
+        }
+    }
+
+    pub const fn khz(self) -> u32 {
+        match self {
+            Self::UpTo100k(f) | Self::UpTo400k(f) => f,
+        }
+    }
+}
+
+riot_rs_shared_types::impl_i2c_from_frequency_up_to!();
+
+pub(crate) fn init(peripherals: &mut crate::OptionalPeripherals) {
+    // Take all I2C peripherals and do nothing with them.
+    cfg_if::cfg_if! {
+        if #[cfg(context = "rp2040")] {
+            let _ = peripherals.I2C0.take().unwrap();
+            let _ = peripherals.I2C1.take().unwrap();
+        } else {
+            compile_error!("this RP chip is not supported");
+        }
+    }
+}
+
+macro_rules! define_i2c_drivers {
+    ($( $interrupt:ident => $peripheral:ident ),* $(,)?) => {
+        $(
+            /// Peripheral-specific I2C driver.
+            pub struct $peripheral {
+                twim: embassy_rp::i2c::I2c<'static, peripherals::$peripheral, embassy_rp::i2c::Async>,
+            }
+
+            impl $peripheral {
+                #[must_use]
+                pub fn new(
+                    sda_pin: impl Peripheral<P: SdaPin<peripherals::$peripheral>> + 'static,
+                    scl_pin: impl Peripheral<P: SclPin<peripherals::$peripheral>> + 'static,
+                    config: Config,
+                ) -> I2c {
+                    let mut i2c_config = embassy_rp::i2c::Config::default();
+                    i2c_config.frequency = config.frequency.khz();
+
+                    bind_interrupts!(
+                        struct Irqs {
+                            $interrupt => InterruptHandler<peripherals::$peripheral>;
+                        }
+                    );
+
+                    // Make this struct a compile-time-enforced singleton: having multiple statics
+                    // defined with the same name would result in a compile-time error.
+                    paste::paste! {
+                        #[allow(dead_code)]
+                        static [<PREVENT_MULTIPLE_ $peripheral>]: () = ();
+                    }
+
+                    // FIXME(safety): enforce that the init code indeed has run
+                    // SAFETY: this struct being a singleton prevents us from stealing the
+                    // peripheral multiple times.
+                    let i2c_peripheral = unsafe { peripherals::$peripheral::steal() };
+
+                    // NOTE(arch): even though we handle bus timeout at a higher level as well, it
+                    // does not seem possible to disable the timeout feature on RP.
+                    let i2c = embassy_rp::i2c::I2c::new_async(
+                        i2c_peripheral,
+                        scl_pin,
+                        sda_pin,
+                        Irqs,
+                        i2c_config,
+                    );
+
+                    I2c::$peripheral(Self { twim: i2c })
+                }
+            }
+        )*
+
+        /// Peripheral-agnostic driver.
+        pub enum I2c {
+            $( $peripheral($peripheral), )*
+        }
+
+        impl embedded_hal_async::i2c::ErrorType for I2c {
+            type Error = riot_rs_shared_types::i2c::controller::Error;
+        }
+
+        impl_async_i2c_for_driver_enum!(I2c, $( $peripheral ),*);
+    }
+}
+
+// We cannot impl From because both types are external to this crate.
+fn from_error(err: embassy_rp::i2c::Error) -> riot_rs_shared_types::i2c::controller::Error {
+    use embassy_rp::i2c::{AbortReason, Error::*};
+
+    use riot_rs_shared_types::i2c::controller::{Error, NoAcknowledgeSource};
+
+    match err {
+        Abort(reason) => match reason {
+            AbortReason::NoAcknowledge => Error::NoAcknowledge(NoAcknowledgeSource::Unknown),
+            AbortReason::ArbitrationLoss => Error::ArbitrationLoss,
+            AbortReason::TxNotEmpty(_) => Error::Other,
+            AbortReason::Other(_) => Error::Other,
+        },
+        InvalidReadBufferLength => Error::Other,
+        InvalidWriteBufferLength => Error::Other,
+        AddressOutOfRange(_) => Error::Other,
+        AddressReserved(_) => Error::Other,
+    }
+}
+
+// Define a driver per peripheral
+define_i2c_drivers!(
+    I2C0_IRQ => I2C0,
+    I2C1_IRQ => I2C1,
+);

future-proof-iot/RIOT-rust

src/riot-rs-nrf/src/i2c/controller.rs
@@ -1,0 +1,200 @@
+use embassy_nrf::{
+    bind_interrupts,
+    gpio::Pin as GpioPin,
+    peripherals,
+    twim::{InterruptHandler, Twim},
+    Peripheral,
+};
+use riot_rs_embassy_common::impl_async_i2c_for_driver_enum;
+
+/// I2C bus configuration.
+#[non_exhaustive]
+#[derive(Clone)]
+pub struct Config {
+    pub frequency: Frequency,
+    pub sda_pullup: bool,
+    pub scl_pullup: bool,
+    pub sda_high_drive: bool,
+    pub scl_high_drive: bool,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            frequency: Frequency::_100k,
+            sda_pullup: false,
+            scl_pullup: false,
+            sda_high_drive: false,
+            scl_high_drive: false,
+        }
+    }
+}
+
+/// I2C bus frequency.
+// NOTE(arch): the datasheets only mention these frequencies.
+#[cfg(any(context = "nrf52840", context = "nrf5340"))]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub enum Frequency {
+    /// Standard mode.
+    _100k,
+    #[cfg(context = "nrf5340")]
+    _250k,
+    /// Fast mode.
+    _400k,
+    // FIXME(embassy): the upstream Embassy crate does not support this frequency
+    // #[cfg(context = "nrf5340")]
+    // _1M,
+}
+
+impl Frequency {
+    pub const fn first() -> Self {
+        Self::_100k
+    }
+
+    pub const fn last() -> Self {
+        Self::_400k
+    }
+
+    pub const fn next(self) -> Option<Self> {
+        match self {
+            #[cfg(not(context = "nrf5340"))]
+            Self::_100k => Some(Self::_400k),
+            #[cfg(context = "nrf5340")]
+            Self::_100k => Some(Self::_250k),
+            #[cfg(context = "nrf5340")]
+            Self::_250k => Some(Self::_400k),
+            Self::_400k => None,
+        }
+    }
+
+    pub const fn prev(self) -> Option<Self> {
+        match self {
+            Self::_100k => None,
+            #[cfg(context = "nrf5340")]
+            Self::_250k => Some(Self::_100k),
+            #[cfg(not(context = "nrf5340"))]
+            Self::_400k => Some(Self::_100k),
+            #[cfg(context = "nrf5340")]
+            Self::_400k => Some(Self::_250k),
+        }
+    }
+
+    pub const fn khz(self) -> u32 {
+        match self {
+            Self::_100k => 100,
+            #[cfg(context = "nrf5340")]
+            Self::_250k => 250,
+            Self::_400k => 400,
+        }
+    }
+}
+
+riot_rs_embassy_common::impl_i2c_from_frequency!();
+
+impl From<Frequency> for embassy_nrf::twim::Frequency {
+    fn from(freq: Frequency) -> Self {
+        match freq {
+            Frequency::_100k => embassy_nrf::twim::Frequency::K100,
+            #[cfg(context = "nrf5340")]
+            Frequency::_250k => embassy_nrf::twim::Frequency::K250,
+            Frequency::_400k => embassy_nrf::twim::Frequency::K400,
+        }
+    }
+}
+
+macro_rules! define_i2c_drivers {
+    ($( $interrupt:ident => $peripheral:ident ),* $(,)?) => {
+        $(
+            /// Peripheral-specific I2C driver.
+            pub struct $peripheral {
+                twim: Twim<'static, peripherals::$peripheral>,
+            }
+
+            impl $peripheral {
+                #[expect(clippy::new_ret_no_self)]
+                #[must_use]
+                pub fn new(
+                    sda_pin: impl Peripheral<P: GpioPin> + 'static,
+                    scl_pin: impl Peripheral<P: GpioPin> + 'static,
+                    config: Config,
+                ) -> I2c {
+                    let mut twim_config = embassy_nrf::twim::Config::default();
+                    twim_config.frequency = config.frequency.into();
+                    twim_config.sda_pullup = config.sda_pullup;
+                    twim_config.scl_pullup = config.scl_pullup;
+                    twim_config.sda_high_drive = config.sda_high_drive;
+                    twim_config.scl_high_drive = config.scl_high_drive;
+
+                    bind_interrupts!(
+                        struct Irqs {
+                            $interrupt => InterruptHandler<peripherals::$peripheral>;
+                        }
+                    );
+
+                    // Make this struct a compile-time-enforced singleton: having multiple statics
+                    // defined with the same name would result in a compile-time error.
+                    paste::paste! {
+                        #[allow(dead_code)]
+                        static [<PREVENT_MULTIPLE_ $peripheral>]: () = ();
+                    }
+
+                    // FIXME(safety): enforce that the init code indeed has run
+                    // SAFETY: this struct being a singleton prevents us from stealing the
+                    // peripheral multiple times.
+                    let twim_peripheral = unsafe { peripherals::$peripheral::steal() };
+
+                    // NOTE(arch): the I2C peripheral and driver do not have any built-in timeout,
+                    // we implement it at a higher level, not in this arch-specific module.
+                    let twim = Twim::new(twim_peripheral, Irqs, sda_pin, scl_pin, twim_config);
+
+                    I2c::$peripheral(Self { twim })
+                }
+            }
+        )*
+
+        /// Peripheral-agnostic driver.
+        pub enum I2c {
+            $( $peripheral($peripheral), )*
+        }
+
+        impl embedded_hal_async::i2c::ErrorType for I2c {
+            type Error = riot_rs_embassy_common::i2c::controller::Error;
+        }
+
+        impl_async_i2c_for_driver_enum!(I2c, $( $peripheral ),*);
+    }
+}
+
+// We cannot impl From because both types are external to this crate.
+fn from_error(err: embassy_nrf::twim::Error) -> riot_rs_embassy_common::i2c::controller::Error {
+    use embassy_nrf::twim::Error::*;
+
+    use riot_rs_embassy_common::i2c::controller::{Error, NoAcknowledgeSource};
+
+    match err {
+        TxBufferTooLong => Error::Other,
+        RxBufferTooLong => Error::Other,
+        Transmit => Error::Other,
+        Receive => Error::Other,
+        BufferNotInRAM => Error::Other,
+        AddressNack => Error::NoAcknowledge(NoAcknowledgeSource::Address),
+        DataNack => Error::NoAcknowledge(NoAcknowledgeSource::Data),
+        Overrun => Error::Overrun,
+        Timeout => Error::Timeout,
+        _ => Error::Other,
+    }
+}
+
+// FIXME: support other nRF archs
+// Define a driver per peripheral
+#[cfg(context = "nrf52840")]
+define_i2c_drivers!(
+    SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0 => TWISPI0,
+    SPIM1_SPIS1_TWIM1_TWIS1_SPI1_TWI1 => TWISPI1,
+);
+#[cfg(context = "nrf5340")]
+define_i2c_drivers!(
+    SERIAL0 => SERIAL0,
+    SERIAL1 => SERIAL1,
+);

src/riot-rs-rp/src/i2c/controller.rs
@@ -1,0 +1,189 @@
+use embassy_rp::{
+    bind_interrupts,
+    i2c::{InterruptHandler, SclPin, SdaPin},
+    peripherals, Peripheral,
+};
+use riot_rs_embassy_common::impl_async_i2c_for_driver_enum;
+
+const KHZ_TO_HZ: u32 = 1000;
+
+/// I2C bus configuration.
+// We do not provide configuration for internal pull-ups as the RP2040 datasheet mentions in
+// section 4.3.1.3 that the GPIO used should have pull-ups enabled.
+#[derive(Clone)]
+#[non_exhaustive]
+pub struct Config {
+    pub frequency: Frequency,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            frequency: Frequency::UpTo100k(100),
+        }
+    }
+}
+
+/// I2C bus frequency.
+// FIXME(embassy): fast mode plus is supported by hardware but requires additional configuration
+// that Embassy does not seem to currently provide.
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+#[repr(u32)]
+pub enum Frequency {
+    /// Standard mode.
+    UpTo100k(u32), // FIXME: use a range integer?
+    /// Fast mode.
+    UpTo400k(u32), // FIXME: use a range integer?
+}
+
+impl Frequency {
+    pub const fn first() -> Self {
+        Self::UpTo100k(1)
+    }
+
+    pub const fn last() -> Self {
+        Self::UpTo400k(400)
+    }
+
+    pub const fn next(self) -> Option<Self> {
+        match self {
+            Self::UpTo100k(f) => {
+                if f < 100 {
+                    // NOTE(no-overflow): `f` is small enough due to if condition
+                    Some(Self::UpTo100k(f + 1))
+                } else {
+                    Some(Self::UpTo400k(self.khz() + 1))
+                }
+            }
+            Self::UpTo400k(f) => {
+                if f < 400 {
+                    // NOTE(no-overflow): `f` is small enough due to if condition
+                    Some(Self::UpTo400k(f + 1))
+                } else {
+                    None
+                }
+            }
+        }
+    }
+
+    pub const fn prev(self) -> Option<Self> {
+        match self {
+            Self::UpTo100k(f) => {
+                if f > 1 {
+                    // NOTE(no-overflow): `f` is large enough due to if condition
+                    Some(Self::UpTo100k(f - 1))
+                } else {
+                    None
+                }
+            }
+            Self::UpTo400k(f) => {
+                if f > 100 + 1 {
+                    // NOTE(no-overflow): `f` is large enough due to if condition
+                    Some(Self::UpTo400k(f - 1))
+                } else {
+                    Some(Self::UpTo100k(self.khz() - 1))
+                }
+            }
+        }
+    }
+
+    pub const fn khz(self) -> u32 {
+        match self {
+            Self::UpTo100k(f) | Self::UpTo400k(f) => f,
+        }
+    }
+}
+
+riot_rs_embassy_common::impl_i2c_from_frequency_up_to!();
+
+macro_rules! define_i2c_drivers {
+    ($( $interrupt:ident => $peripheral:ident ),* $(,)?) => {
+        $(
+            /// Peripheral-specific I2C driver.
+            pub struct $peripheral {
+                twim: embassy_rp::i2c::I2c<'static, peripherals::$peripheral, embassy_rp::i2c::Async>,
+            }
+
+            impl $peripheral {
+                #[expect(clippy::new_ret_no_self)]
+                #[must_use]
+                pub fn new(
+                    sda_pin: impl Peripheral<P: SdaPin<peripherals::$peripheral>> + 'static,
+                    scl_pin: impl Peripheral<P: SclPin<peripherals::$peripheral>> + 'static,
+                    config: Config,
+                ) -> I2c {
+                    let mut i2c_config = embassy_rp::i2c::Config::default();
+                    i2c_config.frequency = config.frequency.khz() * KHZ_TO_HZ;
+
+                    bind_interrupts!(
+                        struct Irqs {
+                            $interrupt => InterruptHandler<peripherals::$peripheral>;
+                        }
+                    );
+
+                    // Make this struct a compile-time-enforced singleton: having multiple statics
+                    // defined with the same name would result in a compile-time error.
+                    paste::paste! {
+                        #[allow(dead_code)]
+                        static [<PREVENT_MULTIPLE_ $peripheral>]: () = ();
+                    }
+
+                    // FIXME(safety): enforce that the init code indeed has run
+                    // SAFETY: this struct being a singleton prevents us from stealing the
+                    // peripheral multiple times.
+                    let i2c_peripheral = unsafe { peripherals::$peripheral::steal() };
+
+                    // NOTE(arch): even though we handle bus timeout at a higher level as well, it
+                    // does not seem possible to disable the timeout feature on RP.
+                    let i2c = embassy_rp::i2c::I2c::new_async(
+                        i2c_peripheral,
+                        scl_pin,
+                        sda_pin,
+                        Irqs,
+                        i2c_config,
+                    );
+
+                    I2c::$peripheral(Self { twim: i2c })
+                }
+            }
+        )*
+
+        /// Peripheral-agnostic driver.
+        pub enum I2c {
+            $( $peripheral($peripheral), )*
+        }
+
+        impl embedded_hal_async::i2c::ErrorType for I2c {
+            type Error = riot_rs_embassy_common::i2c::controller::Error;
+        }
+
+        impl_async_i2c_for_driver_enum!(I2c, $( $peripheral ),*);
+    }
+}
+
+// We cannot impl From because both types are external to this crate.
+fn from_error(err: embassy_rp::i2c::Error) -> riot_rs_embassy_common::i2c::controller::Error {
+    use embassy_rp::i2c::{AbortReason, Error::*};
+
+    use riot_rs_embassy_common::i2c::controller::{Error, NoAcknowledgeSource};
+
+    match err {
+        Abort(reason) => match reason {
+            AbortReason::NoAcknowledge => Error::NoAcknowledge(NoAcknowledgeSource::Unknown),
+            AbortReason::ArbitrationLoss => Error::ArbitrationLoss,
+            AbortReason::TxNotEmpty(_) => Error::Other,
+            AbortReason::Other(_) => Error::Other,
+        },
+        InvalidReadBufferLength => Error::Other,
+        InvalidWriteBufferLength => Error::Other,
+        AddressOutOfRange(_) => Error::Other,
+        AddressReserved(_) => Error::Other,
+    }
+}
+
+// Define a driver per peripheral
+define_i2c_drivers!(
+    I2C0_IRQ => I2C0,
+    I2C1_IRQ => I2C1,
+);